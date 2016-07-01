2604301819e7519e5ad1fadcc67c1086a6315546
丁一鸣 <315129552@qq.com>
2016-07-01 10:19:10 +0800

    将index.html中组件标签移入Main.js

--- webpackdemos/demo5/src/main.js
+++ webpackdemos/demo5/src/main.js
@@ -3,8 +3,9 @@ var Vue = require('vue')
 var testComponent = require("./components/test/test")
 
 new Vue({
-	el: '#app',
+	el: 'app',
 	components: {
 		"test": testComponent
-	}
+	},
+	template: '<test></test>'
 })
