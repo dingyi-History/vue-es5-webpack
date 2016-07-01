e56005d3acc1a20c8fedcd7d2703babcac4365a5
丁一鸣 <315129552@qq.com>
2016-07-01 10:16:34 +0800

    完成目标5：将Inde.html中的标签移入test组件中去

--- webpackdemos/demo5/src/main.js
+++ webpackdemos/demo5/src/main.js
@@ -1,25 +1,9 @@
 var Vue = require('vue')
-require('./css/list.css')
 
 var testComponent = require("./components/test/test")
 
 new Vue({
-	name: "mainjs",
 	el: '#app',
-	data: {
-		msg: "来自又一个vue的消息，也来自 webpack-dev-server",
-		list: [
-		  "总",
-		  "觉",
-		  "得",
-		  "webpack-dev-server",
-		  "比",
-		  "broswersync",
-		  "热加载",
-		  "慢一些"
-		],
-		other: "然后另一个情况是,根HTML不进行热加载"
-	},
 	components: {
 		"test": testComponent
 	}
