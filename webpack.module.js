faf1b028a4fe5021557e356c4ee6722ed9dbee3c
丁一鸣 <315129552@qq.com>
2016-07-03 20:34:45 +0800

    update

--- webpack.module.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-		loaders: [
-			{ test: /\.html$/,loader: 'html-loader' },
-			{ test: /\.css$/,loader: "style-loader!css-loader!postcss-loader" }
-		],
-		postcss: function () {
-        	return [require('autoprefixer'), require('precss')];
-        }
-}
